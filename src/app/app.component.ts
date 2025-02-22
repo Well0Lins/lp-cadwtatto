f6a4144afd19863b2f3fe7a8df4873b186c09af4
Well Lins <wellingtonlinsdev@outlook.com>
2025-02-22 13:05:05 -0300

    Feat: Create estructura SCSS in project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet],
+  template: `<router-outlet></router-outlet>`,
 })
-export class AppComponent {
-  title = 'FrontAppSanidadeWorld';
-}
+export class AppComponent {}
