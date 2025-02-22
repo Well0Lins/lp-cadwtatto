bf054105512da847b747f2336748f018fbbd3d58
Well Lins <wellingtonlinsdev@outlook.com>
2025-02-22 13:14:56 -0300

    Feat: Create estructura Home and components

---
+++ src/app/home/banner/banner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner',
+  imports: [],
+  templateUrl: './banner.component.html',
+  styleUrl: './banner.component.scss'
+})
+export class BannerComponent {
+  bannerText = 'Confira nossos últimos eventos e fotos!';
+}
